f5c3d121f9c0ffe4bf233eded9d5939ab2cfecb9
XBGe <gexinbin@gmail.com>
2021-05-26 22:51:15 +1000

    accordtion practic

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        id: 1,
+        title: 'Why do you want to work here?',
+        info:
+            'Interviewers often ask this question as a way to determine whether or not you took time to research the company and to learn why you see yourself as a good fit. The best way to prepare for this question is to do your homework and learn about the products, services, mission, history and culture of the workplace. In your answer, mention the aspects of the company that appeal to you and align with your career goals. Explain why you’re looking for these things in an employer'
+    },
+    {
+        id: 2,
+        title: 'What makes you unique?',
+        info:
+            'Employers often ask this question to identify why you might be more qualified than other candidates they’re interviewing. To answer, focus on why hiring you would benefit the employer. Since you don’t know the other applicants, it can be challenging to think about your answer in relation to them. Addressing why your background makes you a good fit will let employers know why your traits and qualifications make you well prepared.'
+    },
+    {
+        id: 3,
+        title: 'What interests you about this role?',
+        info:
+            'Like the previous question, hiring managers often include this question to make sure you understand the role, and to give you the opportunity to highlight your relevant skills. In addition to thoroughly reading the job description, it can be helpful to compare the role requirements against your skills and experience. Choose a few things you particularly enjoy or excel at, and focus on those in your answer.'
+    },
+    {
+        id: 4,
+        title: 'What motivates you?',
+        info:
+            'Employers ask this question to gauge your level of self-awareness and ensure your sources of motivation align with the role. To answer, be as specific as possible, provide real-life examples and tie your answer back to the job role.'
+    },
+    {
+        id: 5,
+        title: 'What are you passionate about?',
+        info:
+            'Much like the previous question about motivation, employers might ask what you are passionate about to better understand what drives you and what you care most deeply about. This can both help them understand whether you are a good fit for the role and if it fits into your larger goals. To answer, choose something you are genuinely passionate about, explain why you’re passionate about it, give examples of how you’ve pursued this passion and relate it back to the job.'
+    }
+    
+];
+export default questions;
---
+++ src/singleQuestion.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+
+
+const Question = ({title, info}) => {
+    const [showInfo, setShowInfo] = useState(false);
+    const handleShow = () => {
+        setShowInfo(!showInfo);
+    }
+    return(
+       <article className="question"> 
+           <header>
+               <h4>{title}</h4>
+               <button className="btn" onClick={handleShow}>{showInfo? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
+           </header>
+           {showInfo && <p>{info}</p>}
+       </article>
+    );
+}
+
+export default Question;
